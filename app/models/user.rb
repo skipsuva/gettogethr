dc0bfe9ac114356cc456daf48a3ef05f818cb32c
skipsuva <skip.suva@gmail.com>
2016-01-27 18:03:46 -0500

    add model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :interests, dependent: :destroy
+  has_many :gatherings, through: :interests
+
+  has_many :own_gatherings, foreign_key: 'owner_id'
+
 end
