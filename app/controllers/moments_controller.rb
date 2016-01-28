2af5d9f876e3c57ff86b9bb27230302356167586
flats <dave@robador.com>
2016-01-28 17:45:46 -0500

    create moment action, validate time presence, show errors in layout

--- app/controllers/moments_controller.rb
+++ app/controllers/moments_controller.rb
@@ -1,19 +1,21 @@
 class MomentsController < ApplicationController
   def create
-    @moment = Moment.new()
-    # @gathering = Gathering.new(gathering_params)
-    # @gathering.owner = current_user
-    # @gathering.users << current_user
+    @moment = Moment.new(moment_params.merge({
+      gathering_id: params['gathering_id'] }))
+    @gathering = Gathering.find(params['gathering_id'])
 
-    # respond_to do |format|
-    #   if @moment.save
-    #     format.html { redirect_to @gathering, notice: 'Gathering was successfully created.' }
-    #     format.json { render :show, status: :created, location: @gathering }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @gathering.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    respond_to do |format|
+      if @moment.save
+        format.html { redirect_to @gathering, notice: 'Moment was successfully created.' }
+        format.json { render :show, status: :created, location: @gathering }
+      else
+        format.html {
+          flash.now[:error] = 'Time ' + @moment.errors.messages[:time].join(", ")
+          render 'gatherings/show'
+        }
+        format.json { render json: @gathering.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
