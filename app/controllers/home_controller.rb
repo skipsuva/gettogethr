cec74f02543ee264582aad5b26c79ce69c421a5c
flats <dave@robador.com>
2016-01-29 12:31:08 -0500

    delete duplicate notice display in gatherings index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate!
-  
+
   def index
-    redirect_to gatherings_url
+    redirect_to gatherings_url if logged_in?
   end
 end
