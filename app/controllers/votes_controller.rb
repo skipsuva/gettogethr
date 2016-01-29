c1ce0a4d24ff44030a59e71e94757d4bc1cd0ec0
TimoSci <me@timosci.net>
2016-01-29 12:57:43 -0500

    built basic voting functionality and added vote box to moments

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,33 +5,33 @@ class VotesController < ApplicationController
     #TODO Sanitize input
     votable = Object.const_get(params[:votable_class]).find(params[:votable_id])
     gathering = Gathering.find(params[:gathering_id])
-    binding.pry
 
-    #  case vote = Vote.find_by(user:current_user,votable:votable)
-     #
-    #  when #vote exists
-    # when params[:thumbage]
-    #   case #value is the same
-    #     #delete vote
-    #   case #value is different
-    #     #change value of vote
-    #   end
-    # when #vote doesn't exist
-    #   # create new vote with value
-    # end
+
+    if vote = Vote.find_by(user:current_user,votable:votable)
+      if params[:thumbage].to_i == vote.value.to_i
+        vote.destroy
+      else
+        vote.update(value:params[:thumbage])
+      end
+    else #vote doesn't exist
+      new_vote = Vote.create(user: current_user, value: params[:thumbage])
+      votable.votes << new_vote
+    end
+
+    redirect_to gathering
 
   end
 
   private
 
-  def new(user:user, value: value)
-    new_vote = Vote.create(user: user, value: value)
-    self.votes << new_vote
-  end
-
-  def unvote(user)
-    vote = Vote.find_by(user: user, votable: self)
-    vote.destroy
-  end
+  # def new(user:user, value: value)
+  #   new_vote = Vote.create(user: user, value: value)
+  #   self.votes << new_vote
+  # end
+  #
+  # def unvote(user)
+  #   vote = Vote.find_by(user: user, votable: self)
+  #   vote.destroy
+  # end
 
 end
