a5311a6c7ae8280ed86a8920aa7871be2f1563ea
TimoSci <me@timosci.net>
2016-02-01 11:24:06 -0500

    adde place list to gatherings show page

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,34 @@
+class PlacesController < ApplicationController
+
+  def create
+    @place = Place.new(place_params.merge({
+      place_id: params['place_id'] }))
+    @gathering = Gathering.find(params['gathering_id'])
+
+    respond_to do |format|
+      if @place.save
+        format.html { redirect_to @gathering, notice: 'Place was successfully created.' }
+        format.json { render :show, status: :created, location: @gathering }
+      else
+        format.html {
+          flash.now[:error] = 'Place ' + @place.errors.messages[:time].join(", ")
+          render 'gatherings/show'
+        }
+        format.json { render json: @gathering.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    gathering = Gathering.find(params[:id])
+     Place.find(params[:place_id]).destroy
+    redirect_to gathering
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name)
+  end
+
+end
