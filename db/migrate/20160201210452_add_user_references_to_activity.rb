4f04f35667f2dfdcdaf622f92b93811abfb542c3
flats <dave@robador.com>
2016-02-01 16:14:03 -0500

    add migration file for user refs

--- db/migrate/20160201210452_add_user_references_to_activity.rb
+++ db/migrate/20160201210452_add_user_references_to_activity.rb
@@ -1,4 +1,7 @@
 class AddUserReferencesToActivity < ActiveRecord::Migration
   def change
+    change_table :activities do |t|
+      t.references :user, index: true, foreign_key: true
+    end
   end
 end
