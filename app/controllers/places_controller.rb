38466962d05417f2f6290c621918ef631ac85462
Skip Suva <skip.suva@gmail.com>
2016-02-01 11:52:53 -0500

    Remove instance vars and add user for places
    Adjust error message display

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,20 +1,20 @@
 class PlacesController < ApplicationController
 
   def create
-    @place = Place.new(place_params.merge({
-      gathering_id: params['gathering_id'] }))
-    @gathering = Gathering.find(params['gathering_id'])
+    place = Place.new(place_params.merge({
+      gathering_id: params['gathering_id'], user_id: current_user.id }))
+    gathering = Gathering.find(params['gathering_id'])
 
     respond_to do |format|
-      if @place.save
-        format.html { redirect_to @gathering, notice: 'Place was successfully created.' }
-        format.json { render :show, status: :created, location: @gathering }
+      if place.save
+        format.html { redirect_to gathering, notice: 'Place was successfully created.' }
+        format.json { render :show, status: :created, location: gathering }
       else
         format.html {
-          flash.now[:error] = 'Place ' + @place.errors.messages[:time].join(", ")
+          flash.now[:error] = place.errors.messages.map { |msg| "<li>#{msg}</li>"}.join('')
           render 'gatherings/show'
         }
-        format.json { render json: @gathering.errors, status: :unprocessable_entity }
+        format.json { render json: gathering.errors, status: :unprocessable_entity }
       end
     end
   end
