9665c739035e4cab89814f7478d853d458132e72
flats <dave@robador.com>
2016-02-01 16:09:41 -0500

    activity/user assoc, user has_many moments/places/etc.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,7 @@
 class Activity < ActiveRecord::Base
   belongs_to :activity_category
   belongs_to :gathering
+  belongs_to :user
   has_many :votes, as: :votable
 
   validates :description, presence: true
