24a0cdcd2099793bf1a3670b3dd9f3e871a3a492
TimoSci <me@timosci.net>
2016-02-02 11:16:10 -0500

    make instance variable in places#create for ajax

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,21 +3,20 @@ class PlacesController < ApplicationController
 
 
   def create
-    place = Place.new(place_params.merge({
+    @place = Place.new(place_params.merge({
       gathering_id: params['gathering_id'], user_id: current_user.id }))
-    gathering = Gathering.find(params['gathering_id'])
+    @gathering = Gathering.find(params['gathering_id'])
 
     respond_to do |format|
-      if place.save
-        format.html { redirect_to gathering, notice: 'Place was successfully created.' }
-        format.json { render :show, status: :created, location: gathering }
+      if @place.save
+        format.html { redirect_to @gathering, notice: 'Place was successfully created.' }
         format.js {}
       else
         format.html {
-          flash.now[:error] = place.errors.messages.map { |msg| "<li>#{msg}</li>"}.join('')
+          flash.now[:error] = @place.errors.messages.map { |msg| "<li>#{msg}</li>"}.join('')
           render 'gatherings/show'
         }
-        format.json { render json: gathering.errors, status: :unprocessable_entity }
+        format.json { render json: @gathering.errors, status: :unprocessable_entity }
       end
     end
   end
