6f2f09749202d4f154e724c860651a9682d712ef
skipsuva <skip.suva@gmail.com>
2016-02-03 11:59:16 -0500

    create lambda literal in find_best

--- app/models/gathering.rb
+++ app/models/gathering.rb
@@ -12,20 +12,38 @@ class Gathering < ActiveRecord::Base
   #   pass in array of gathering.moments
 
   # F1
-  def upvote_ratio #item as arg
-    self.places.select do |place|
-      upvotes = (place.upvotes)
-      downvotes = (place.downvotes)
-      upvotes > downvotes
-    end
+  # def positive_total #item as arg
+  #   self.places.select do |place|
+  #     upvotes = (place.upvotes)
+  #     downvotes = (place.downvotes)
+  #     upvotes > downvotes
+  #   end
+  # end
+
+# other filters:
+
+  def vote_ratio(item_collection)
+
+  end
+
+  def final_pick(item_collection)
+    item_collection.sample
   end
 
 
 
 
-
   def find_best(item)
-    votable = Object.const_get(item)
+    collection = self.send(item.to_s.pluralize)
+
+    positive_total = ->(){
+      collection.select do |item|
+      upvotes = (item.upvotes)
+      downvotes = (item.downvotes)
+      upvotes > downvotes}
+    }
+
+    positive_total.()
 
   end
 
