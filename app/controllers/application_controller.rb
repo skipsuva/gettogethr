918221d8b26770d844425554edb20580d9c33d70
flats <dave@robador.com>
2016-02-06 20:02:48 -0500

    add name fields and name strong param, fix log in redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,14 @@ class ApplicationController < ActionController::Base
     redirect_to gatherings_path, :flash => { :error => "You have not been invited to that gathering." } unless is_interested?(gathering_id)
   end
 
-  def after_sign_in_path_for
+  def after_sign_in_path_for(user)
     gatherings_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
   end
 end
