251e1e9a6ce5d69c1f5b80cbfe3f5ef060a0293c
D. Flaherty <dave@robador.com>
2016-02-07 13:32:28 -0500

    delete comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,13 +14,10 @@ class User < ActiveRecord::Base
   has_many :own_gatherings, foreign_key: 'owner_id'
   has_many :votes
 
-  # has_secure_password
-
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(:email => data["email"]).first
 
-    # Uncomment the section below if you want users to be created if they don't exist
     unless user
         user = User.create(name: data["name"],
            email: data["email"],
