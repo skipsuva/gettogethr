d7551286f598cc65fc970b22d58ec0cee27be9a0
flats <dave@robador.com>
2016-02-06 20:07:27 -0500

    add before_action to enable name strong params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :user_signed_in?
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # def current_user
   #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
