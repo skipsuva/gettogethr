60f693421babfb2835cbd165dccc88cdc51ec0e7
flats <dave@robador.com>
2016-02-06 19:35:32 -0500

    use authenticate_user! helper, comment out old helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
-  before_action :authenticate!
+  before_action :authenticate_user!
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
 
-  def logged_in?
-    !!current_user
-  end
+  # def logged_in?
+  #   !!current_user
+  # end
 
-  def authenticate!
-    redirect_to login_path, notice: "You must be logged in to do that." unless logged_in?
-  end
+  # def authenticate!
+  #   redirect_to login_path, notice: "You must be logged in to do that." unless logged_in?
+  # end
 
   def is_interested?(gathering_id)
     gathering = Gathering.find(gathering_id)
