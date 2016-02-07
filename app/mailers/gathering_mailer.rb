c64244061054937210ddcac8aab16ae9c6c941bd
skipsuva <skip.suva@gmail.com>
2016-02-07 12:30:51 -0500

    create finalize plan mailer route

--- app/mailers/gathering_mailer.rb
+++ app/mailers/gathering_mailer.rb
@@ -7,4 +7,12 @@ class GatheringMailer < ApplicationMailer
     # @url = gathering
     mail(to: @user.email, subject: 'You have been invited to a gathering!')
   end
+
+  def finalize_plan(gathering)
+    @gathering = gathering
+    @current_user = current_user
+    @gathering.users.each do |user|
+      mail(to: user.email, subject: "The gathering #{@gathering.title} has been finalized!")
+    end
+  end
 end
