6901da4cb1e38d96bcd2e9da8c4d82020b09f5a9
flats <dave@robador.com>
2016-02-06 20:08:54 -0500

    clean up routes, delete old controllers and helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,18 +6,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def current_user
-  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  # end
-
-  # def logged_in?
-  #   !!current_user
-  # end
-
-  # def authenticate!
-  #   redirect_to login_path, notice: "You must be logged in to do that." unless logged_in?
-  # end
-
   def is_interested?(gathering_id)
     gathering = Gathering.find(gathering_id)
     gathering.users.include?(current_user)
