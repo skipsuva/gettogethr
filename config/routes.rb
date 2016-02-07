fb48b184532b52c65716d32bbfd1036e66c8b65a
flats <dave@robador.com>
2016-02-07 13:01:53 -0500

    fix user & devise callback routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :users
 
   root 'home#index'
   get 'home/index'
