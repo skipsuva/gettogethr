a055defcdb219960ff06f05f2b9ccb4c3cad2e8a
skipsuva <skip.suva@gmail.com>
2016-02-07 12:04:48 -0500

    add invite user mailer method

--- app/mailers/gathering_mailer.rb
+++ app/mailers/gathering_mailer.rb
@@ -1,2 +1,7 @@
 class GatheringMailer < ApplicationMailer
+
+  def invite_user(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'You have been invited to a gathering!)
 end
