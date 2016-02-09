dd13f38ac3588f1bc4aafbaa9580429e39b67f1c
TimoSci <me@timosci.net>
2016-02-09 07:56:08 -0500

    display closed gatherings in different color

--- app/helpers/gatherings_helper.rb
+++ app/helpers/gatherings_helper.rb
@@ -1,5 +1,18 @@
 module GatheringsHelper
+
   def sort_by_suggestion(collection)
     collection.sort_by { |item| item.suggested? ? 0 : 1 }
   end
+
+  def row_class(gathering)
+    case gathering.aasm_state
+    when "open"
+      "open"
+    when "finalized"
+      "finalized"
+    else
+      ""
+    end
+  end
+
 end
