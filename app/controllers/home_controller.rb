d942a52b9590e4994511be520b1c3d4993952ffc
flats <dave@robador.com>
2016-02-09 12:14:58 -0500

    remove le endpoint

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,4 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
   layout 'application'
-
-  def letsencrypt
-    render text: ENV['LE_CHALLENGE']
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get '/.well-known/acme-challenge/:id' => 'home#letsencrypt'
-
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users
 
